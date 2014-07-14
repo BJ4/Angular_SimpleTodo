44472529e8cf0564713c323e0f153e4c1cb2f011
Chris Chen <chris085566@gmail.com>
2014-07-14 23:58:20 +0800

    implement angular get

---
+++ public/core.js
@@ -0,0 +1,14 @@
+var myTodo = angular.module('MyTodo', []);
+
+function mainController($scope, $http) {
+	$scope.formData = {};
+
+	$http.get('/api/todos')
+		.success(function(data) {
+			$scope.todos = data;
+			console.log(data);
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+}
